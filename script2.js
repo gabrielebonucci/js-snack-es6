4c2bfc088a02949b251b8703274bea6d2e1f5e33
Gabriele Bonucci <g.bonucci99@gmail.com>
2025-09-09 16:38:08 +0200

    esercizio completato

--- script2.js
+++ script2.js
@@ -1,4 +1,5 @@
 // creare un array di oggetti (squadre di calcio)
+console.log("--- Snack 2 ---");
 
 const squadreDiCalcio = [
     {
@@ -36,5 +37,18 @@ for (let i = 0; i<squadreDiCalcio.length; i++) {
     squadraCorrente.falliSubiti = generaNumeroRandom(50,500);
 
 }
-console.log("Array aggiornato con il ciclo for:");
-console.log(squadreDiCalcio);
+// creare un nuovo array per visualizzare in console solo il nome e i falli subiti
+
+const squadreNomiFalli = [];
+for (let i = 0; i<squadreDiCalcio.length; i++) {
+    const squadraCorrente = squadreDiCalcio[i];
+    const nuovoOggetto = {
+        nome: squadraCorrente.nome,
+        falliSubiti: squadraCorrente.falliSubiti
+    };
+    squadreNomiFalli.push(nuovoOggetto);
+
+}
+
+console.log("Array finale con solo nomi e falli subiti:");
+console.log(squadreNomiFalli);
