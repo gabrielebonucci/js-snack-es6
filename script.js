6463723ef4081d25a1c6123d33d26d8239dc2f44
Gabriele Bonucci <g.bonucci99@gmail.com>
2025-09-09 15:45:00 +0200

    snack bici completato

--- script.js
+++ script.js
@@ -2,8 +2,8 @@
 
 const biciDaCorsa = [
     {
-         nome: 'Bianchi specialissima',
-         peso: 6.7 //kg
+        nome: 'Bianchi specialissima',
+        peso: 6.7 //kg
    
     },
     {
@@ -19,4 +19,24 @@ const biciDaCorsa = [
         peso: 6.5
     },
 
-]
+];
+
+// Trovare la bici con peso minore.
+// Inizializziamo una variabile per contenere la bici più leggera trovata finora.
+// Partiamo assumendo che la prima bici sia la più leggera.
+let biciPiuLeggera = biciDaCorsa[0];
+
+// Cicliamo attraverso l'array (partendo dal secondo elemento, indice 1,
+// perché il primo lo abbiamo già memorizzato)
+
+for (let i = 1; i < biciDaCorsa.length; i++) {
+    const biciCorrente = biciDaCorsa[i];
+
+    if (biciCorrente.peso < biciPiuLeggera.peso) {
+        biciPiuLeggera = biciCorrente
+    }
+}
+// Stampare a schermo (in console) la bici con peso minore.
+console.log("--- Snack 1 ---");
+console.log("La bici più leggera è:");
+console.log(biciPiuLeggera);
