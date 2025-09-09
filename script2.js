c0418aa24afd1c59f6ac4a9e18c1159edb19d4d5
Gabriele Bonucci <g.bonucci99@gmail.com>
2025-09-09 16:03:16 +0200

    inizio secondo snack

---
+++ script2.js
@@ -0,0 +1,27 @@
+// creare un array di oggetti (squadre di calcio)
+
+const squadreDiCalcio = [
+    {
+        nome: 'Pescara',
+        puntiFatti: 0,
+        falliSubiti: 0,
+    },
+    {
+        nome: 'Milan',
+        puntiFatti: 0,
+        falliSubiti: 0,
+    },
+    {
+        nome: 'Trastevere calcio',
+        puntiFatti: 0,
+        falliSubiti: 0,
+    },
+    {
+        nome: 'fczetanapoli',
+        puntiFatti: 0,
+        falliSubiti: 0,
+    },
+
+
+]
+
