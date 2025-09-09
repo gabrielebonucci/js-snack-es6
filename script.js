dfd34e5802396217e0b064afa77d4702a2e376c3
Gabriele Bonucci <g.bonucci99@gmail.com>
2025-09-09 15:35:03 +0200

    Creato array di oggetti

---
+++ script.js
@@ -0,0 +1,22 @@
+// creare un array di oggetti (bici da corsa)
+
+const biciDaCorsa = [
+    {
+         nome: 'Bianchi specialissima',
+         peso: 6.7 //kg
+   
+    },
+    {
+        nome: 'Trek Emonda SLR 9',
+        peso: 6.3
+    },
+    {
+        nome: 'Cannondale SuperSix EVO Hi-MOD',
+        peso: 7.0
+    },
+    {
+        nome: 'Specialized S-Works Tarmac SL7',
+        peso: 6.5
+    },
+
+]
